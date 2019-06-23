dd7731333f6fc7b3e7b94640c3ba7521eb821632
Dominic <dominic.laube@gmail.com>
2019-06-23 15:08:56 +0200

    add reset button, add comments on event listener

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gridContainer = document.querySelector("#gridContainer");
 //gridDiv.classList.add("gridDiv");
 let createDiv;
-const GRIDMAKER = makeGrid(16);
+let gridMaker = makeGrid(64);
 let gridDiv = document.getElementsByClassName("gridDiv");
 
 
@@ -18,7 +18,20 @@ function changeColor() {
     this.style.backgroundColor = "black";
     return;
 }
+function resetGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    let prompt = window.prompt("Enter grid size");
+    gridMaker = makeGrid(prompt);
+    
+    return;
+}
 
+//add event listener for each grid element
+//Does not work for dynamically added elements! Better use Event delegation.
 for (var i = 0; i < gridDiv.length; i++) {
     gridDiv[i].addEventListener("mouseover", changeColor, false);
 }
+
+document.getElementById("reset").addEventListener("click", resetGrid, false);
